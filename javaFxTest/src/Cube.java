44bbc70d1775502a5baeca8344cac9c4c2a235c7
Vincent Wei <vincentwei.98@gmail.com>
2020-02-27 14:45:39 +0100

    classe Cube de devFelix

--- javaFxTest/src/Cube.java
+++ javaFxTest/src/Cube.java
@@ -1,3 +1,5 @@
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.geometry.Bounds;
 import javafx.scene.paint.Color;
 import java.util.Random;
 import javafx.scene.paint.PhongMaterial;
@@ -5,16 +7,23 @@ import javafx.scene.shape.Box;
 
 public class Cube extends Box{
 	Color color;
-		
-	public Cube(Color c, int w, int h, int d) {
+	static int numeroCube = 0;
+	int id;
+
+
+
+	public Cube(Color c, double w, double h, double d) {
 		super(w,h,d);
 		color = c;
 		setMaterial(new PhongMaterial(c));
+		numeroCube++;
+		id = numeroCube;
 	}
+
 	public Cube(Color c) {
 		this(c,1,1,1);
 	}
-		
+
 	public Cube() {
 		super(1,1,1);
 	}
@@ -25,6 +34,20 @@ public class Cube extends Box{
 	public void setColor(Color c) {
 		setMaterial(new PhongMaterial(c));
 	}
+	public Color getColor() {
+		return color;
 
+	}
 
+	public boolean equals(Cube c) {
+		return this.id == c.id;
+	}
+
+	public boolean isColliding(Cube cube) {
+		return getBoundsInParent().getMaxX()-0.01 >= cube.getBoundsInParent().getMinX()
+				&& getBoundsInParent().getMinX()+0.01 <= cube.getBoundsInParent().getMaxX()
+				&& getBoundsInParent().getMaxY()-0.01	>= cube.getBoundsInParent().getMinY()
+				&& getBoundsInParent().getMinY()	+0.01<= cube.getBoundsInParent().getMaxY();
+
+	}
 }
