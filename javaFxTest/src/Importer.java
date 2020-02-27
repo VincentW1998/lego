ebb85e09a1c7fb39fe6ca3ac4cda21ea42c39cf3
Vincent Wei <vincentwei.98@gmail.com>
2020-02-27 13:26:12 +0100

    debut loadFrom

--- javaFxTest/src/Importer.java
+++ javaFxTest/src/Importer.java
@@ -1,4 +1,5 @@
 import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -6,8 +7,12 @@ import java.io.IOException;
 
 public class Importer {
 
+
     public static void loadFrom(File f) throws IOException {
         JSONObject json = new JSONObject(FileUtils.readFileToString(f,"utf-8"));
-
+        JSONArray construction = json.getJSONArray("construction");
+        for(int i = 0; i < construction.length(); i++){
+            JSONObject cube = construction.getJSONObject(i);
+        }
     }
 }
