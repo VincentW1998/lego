c11eed392c96d2735cfef745147325eea4b036b1
Kevin LIM <kevinlim75013@gmail.com>
2020-03-04 13:41:24 +0100

    Main ajouter

---
+++ javaFxTest/src/javafxapplication3/JavaFXApplication3.java
@@ -0,0 +1,34 @@
+package javafxapplication3;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author kevinlim
+ */
+public class JavaFXApplication3 extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane root =(AnchorPane) FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
