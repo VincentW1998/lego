6d35a54fff73e187b794c05b1f4602305f7422c2
Kevin LIM <kevinlim75013@gmail.com>
2020-03-12 12:27:58 +0100

    raccourcis clavier

--- javaFxTest/src/javafxapplication3/FXMLDocumentController.java
+++ javaFxTest/src/javafxapplication3/FXMLDocumentController.java
@@ -1,11 +1,13 @@
 package javafxapplication3;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
@@ -14,7 +16,12 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
-
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -29,6 +36,8 @@ public class FXMLDocumentController implements Initializable {
     private TextField width_value;
     @FXML
     private TextField height_value;
+    @FXML
+    private Button hotkeys;
 
     /**
      * Initializes the controller class.
@@ -49,5 +58,15 @@ public class FXMLDocumentController implements Initializable {
         System.out.println("Length : "+length_value.getText());
         System.out.println("Width : "+width_value.getText());
         System.out.println("Height : "+height_value.getText());
+    }
+
+    @FXML
+    void newWindow(ActionEvent event, Stage stage) throws IOException {
+        AnchorPane root =(AnchorPane) FXMLLoader.load(getClass().getResource("hotkeys.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
---
+++ javaFxTest/src/javafxapplication3/hotkeysController.java
@@ -0,0 +1,17 @@
+package javafxapplication3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class hotkeysController {
+
+    @FXML
+    private Button closebutton;
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+}
