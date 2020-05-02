d6e2053353011ae4a839063858a6c0f6618670e5
Vincent Wei <vincentwei.98@gmail.com>
2020-05-02 02:46:57 +0200

    brochures complete, bien dimensionnée

--- ProjetMaven/src/main/java/Brochure.java
+++ ProjetMaven/src/main/java/Brochure.java
@@ -16,7 +16,7 @@ import java.util.LinkedList;
 
 public class Brochure {
 
-    static LinkedList <File> fichierPng = new LinkedList<File>();
+    static LinkedList <File> fichierPng = new LinkedList<File>(); // Liste de file
 
     /* Creer une brochure à partir du decoupage manuel */
 
@@ -53,37 +53,49 @@ public class Brochure {
             group.getChildren().remove(1);
     }
 
+    // creer une brochure PDF à partir de l'alog
     public static void creationBrochureAlgo(SelectionModel selection) {
+        // On creer le dossier assemblage
         File assemblage = new File ("src/main/resources/Brochures/Assemblage");
         assemblage.mkdir();
 
+        // on parcourt la selection de cube
         for(int i = 0; i < selection.listeCubeSelectionne.size(); i++){
+            // on ajoute chaque cube de la selection dans le groupe
             selection.group.getChildren().add(selection.listeCubeSelectionne.get(i));
             selection.listeCubeSelectionne.get(i).setDrawMode(DrawMode.FILL);
+            // on initialise un fichier
             File f = new File("src/main/resources/Brochures/Assemblage/" + "etape "+(i+1)+".png");
-            try {//creer l'image
+            try {//creer un fichier png à partir d'un screenshot de la scene
                 ImageIO.write(SwingFXUtils.fromFXImage(selection.group.getScene().snapshot(null), null), "png", f);
-                fichierPng.add(f);
+                fichierPng.add(f); // on ajoute à la linkedlist de File
             } catch (IOException e) {
                 System.out.println("error PNG");
             }
         }
-        pngToPdf();
+        pngToPdf(); // On transforme la linkedlist de File en un document pdf
     }
 
+    // fonction qui permet de creer un document pdf
     public static void pngToPdf() {
         Document document = new Document();
 
        String input = null;
-        String output = "src/main/resources/Brochures/brochure.pdf";
+        String output = "src/main/resources/Brochures/brochure.pdf"; // path de la brochure
         try {
             FileOutputStream fos = new FileOutputStream(output);
             PdfWriter writer = PdfWriter.getInstance(document, fos);
             writer.open();
             document.open();
+            // on parcout la liste de file, on creer une image et on l'ajoute au document pdf
             for(int i = 0; i < fichierPng.size(); i++) {
                 input = fichierPng.get(i).getPath();
-                document.add(Image.getInstance(input));
+                Image image = Image.getInstance(input);
+                // redimensionne l'image en fonction du document
+                float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
+                        - document.rightMargin()) / image.getWidth()) * 100;
+                image.scalePercent(scaler);
+                document.add(Image.getInstance(image));
             }
             document.close();
             writer.close();
