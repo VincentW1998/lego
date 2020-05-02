8149a2ebf486186c378cc4523ca8d7f6c3729d15
Vincent Wei <vincentwei.98@gmail.com>
2020-05-02 04:56:29 +0200

    brochure algo, suppresion des fichiers une fois termine

--- ProjetMaven/src/main/java/Brochure.java
+++ ProjetMaven/src/main/java/Brochure.java
@@ -1,3 +1,4 @@
+import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -7,8 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.shape.DrawMode;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,8 +15,7 @@ import java.util.LinkedList;
 
 public class Brochure {
 
-    static LinkedList <File> fichierPng = new LinkedList<File>(); // Liste de file
-
+    static LinkedList <Image> listeImages = new LinkedList<Image>();
     /* Creer une brochure à partir du decoupage manuel */
 
     public static void creationBrochure(Scene scene, Group group, SelectionModel selection) {
@@ -50,32 +48,31 @@ public class Brochure {
 
     // creer une brochure PDF à partir de l'alog
     public static void creationBrochureAlgo(SelectionModel selection) {
-        // On creer le dossier assemblage
-        File assemblage = new File ("src/main/resources/Brochures/Assemblage");
-        assemblage.mkdir();
-
         // on parcourt la selection de cube
         for(int i = 0; i < selection.listeCubeSelectionne.size(); i++){
             // on ajoute chaque cube de la selection dans le groupe
             selection.group.getChildren().add(selection.listeCubeSelectionne.get(i));
             selection.listeCubeSelectionne.get(i).setDrawMode(DrawMode.FILL);
             // on initialise un fichier
-            File f = new File("src/main/resources/Brochures/Assemblage/" + "etape "+(i+1)+".png");
-            try {//creer un fichier png à partir d'un screenshot de la scene
+            File f = new File("src/main/resources/Brochures/" + "etape.png");
+            //creer un fichier png à partir d'un screenshot de la scene
+            try {
                 ImageIO.write(SwingFXUtils.fromFXImage(selection.group.getScene().snapshot(null), null), "png", f);
-                fichierPng.add(f); // on ajoute à la linkedlist de File
-            } catch (IOException e) {
+                // on ajoute à la Linkedlist d'Image
+                listeImages.add(Image.getInstance(f.getPath()));
+                f.delete(); // supprimer le fichier png
+            } catch (IOException | BadElementException e) {
                 System.out.println("error PNG");
             }
         }
-        pngToPdf(); // On transforme la linkedlist de File en un document pdf
+        imagesToPdf(); // On transforme la linkedlist de File en un document pdf
     }
 
     // fonction qui permet de creer un document pdf
-    public static void pngToPdf() {
+    public static void imagesToPdf() {
         Document document = new Document();
 
-       String input = null;
+//       String input = null;
         String output = "src/main/resources/Brochures/brochure.pdf"; // path de la brochure
         try {
             FileOutputStream fos = new FileOutputStream(output);
@@ -83,14 +80,12 @@ public class Brochure {
             writer.open();
             document.open();
             // on parcout la liste de file, on creer une image et on l'ajoute au document pdf
-            for(int i = 0; i < fichierPng.size(); i++) {
-                input = fichierPng.get(i).getPath();
-                Image image = Image.getInstance(input);
+            for(int i = 0; i < listeImages.size(); i++) {
                 // redimensionne l'image en fonction du document
                 float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
-                        - document.rightMargin()) / image.getWidth()) * 100;
-                image.scalePercent(scaler);
-                document.add(Image.getInstance(image));
+                        - document.rightMargin()) / listeImages.get(i).getWidth()) * 100;
+                listeImages.get(i).scalePercent(scaler);
+                document.add(Image.getInstance(listeImages.get(i)));
             }
             document.close();
             writer.close();
