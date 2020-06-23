4cf2c4ee1de53d206e5dd91a120d00ae054ad6bb
Vincent Wei <vincentwei.98@gmail.com>
2020-06-23 15:42:53 +0200

    suppresion affichage dimesion fenetre

--- ProjetMaven/src/main/java/Controller.java
+++ ProjetMaven/src/main/java/Controller.java
@@ -406,8 +406,6 @@ public class Controller {
             AnchorPane Apane= loader.load();
             Scene secondScene = new Scene(Apane);
             model.secondStage.setResizable(false); // Ne permet pas de redimensionner la telecommande
-            System.out.println(d);
-            System.out.println(h);
             model.secondStage.setX(d); // a revoir !!!!!
             model.secondStage.setY(h);
             model.secondStage.setScene(secondScene);
