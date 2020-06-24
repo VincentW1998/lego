4cc8dd62b2930b7f51d1f4378a81b94bb20ae879
Kevin LIM <kevinlim75013@gmail.com>
2020-06-24 21:15:54 +0200

    position fenetre primarystage

--- ProjetMaven/src/main/java/Test.java
+++ ProjetMaven/src/main/java/Test.java
@@ -3,6 +3,7 @@ import javafx.application.Platform;
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +21,10 @@ public class Test extends Application implements Initializable {
 
 	public void start(Stage primaryStage) throws Exception {
 
+		Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+		int hauteur = screenDim.height;
+		int longeur = screenDim.width;
+
 		Stage secondStage = new Stage();
 
 
@@ -29,11 +34,12 @@ public class Test extends Application implements Initializable {
 
 		primaryStage.setTitle("Editeur LEGO"); // frame
 		primaryStage.setScene(model.scene);
-		primaryStage.setX(50);
+		primaryStage.setX(0);
 		primaryStage.setY(150);
+		primaryStage.setWidth(longeur - 312);
 //		primaryStage.centerOnScreen();
 		primaryStage.show();
-
+		
 		model.setup();
 
 		primaryStage.setOnHidden(e -> Platform.exit());
